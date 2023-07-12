f9409f1d4e4631bc543d1562cb6d821f9ac70191
rishiraj007 <rishiraj@mounttalent.com>
2023-07-12 15:45:46 +0530

    [Prabhu]Add: Indian States Census Analyser Problem

---
+++ IndianStatesCensusAnalyserProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IndianStatesCensusAnalyserProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indian States Census Analyser Problem!");
+        }
+    }
+}
